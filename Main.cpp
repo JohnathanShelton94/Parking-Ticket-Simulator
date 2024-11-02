f2f53d59384cdbfbba283d418f9c61baf28bbbe2
Johnathan Shelton <js24634@email.vccs.edu>
2024-11-01 21:55:22 -0400

    Revised Main function with tests and comments to prove different cases.

--- Main.cpp
+++ Main.cpp
@@ -6,21 +6,37 @@
 #include "PoliceOfficer.h"
 #include "ParkingTicket.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-                // Test data
-    ParkedCar car("Hyundai", "Sonata", "silver", "A12345", 120);  
-    ParkingMeter meter(60);  
+            // Test data from the table
+    vector<int> minutesParked = { 45, 65, 120, 150, 180, 200, 240, 300, 350 };
+    int minutesPurchased = 60;
+
+            // Officer and car details (sample data)
     PoliceOfficer officer("Mark Smith", "B123456");
+    ParkedCar car("Hyundai", "Sonata", "silver", "A12345");
 
     cout << "Testing ParkingTicket Generation:\n";
-    if (car.getMinutesParked() > meter.getMinutesPurchased()) {
-        ParkingTicket ticket(car, meter, officer);
-        ticket.printTicket();
-    }
-    else {
-        cout << "No parking violation.\n";
+    cout << "---------------------------------\n";
+
+            // Iterate through test cases
+    for (size_t i = 0; i < minutesParked.size(); ++i) {
+        car.setMinutesParked(minutesParked[i]);
+        ParkingMeter meter(minutesPurchased);
+
+        cout << "Test Case " << (i + 1) << ": Car Parked for " << minutesParked[i] << " minutes.\n";
+
+        if (car.getMinutesParked() > meter.getMinutesPurchased()) {
+            ParkingTicket ticket(car, meter, officer);
+            ticket.printTicket();
+        }
+        else {
+            cout << "No parking violation.\n";
+        }
+
+        cout << "---------------------------------\n";
     }
 
     return 0;
